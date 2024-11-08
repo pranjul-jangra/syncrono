88fdbe75a0e32f64e4e0a431604d265efb9a55ce
pranjul jangra <pranjuljan68@gmail.com>
2024-11-08 18:06:38 +0530

    make changes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
+let body = document.querySelector('body');
+let screenWidth = window.innerWidth;
+window.addEventListener('load', () => {
+    if (screenWidth > 880) {
+        body.classList.replace('before:translate-y-0', 'before:translate-y-1/2');
+    } else if (screenWidth <= 880) {
+        body.classList.add('before:hidden');
+    }
+});
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 880) {
+        body.classList.replace('before:translate-y-1/2', 'before:translate-y-0');
+    } else {
+        body.classList.replace('before:translate-y-0', 'before:translate-y-1/2');
+    }
+});
+
+
+
 //                                         STOP WATCH: MANIPULATE YOUR TIME
 
 // DOM getters
